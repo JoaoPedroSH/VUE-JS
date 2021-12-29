2a3edf9f4116e6b51d4afffcb48b9d836e75e46b
Joao Pedro <joaopedro.shalom27@gmail.com>
2021-12-29 15:29:58 -0300

    Home

--- 3_jp_burger/src/App.vue
+++ 3_jp_burger/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Navbar />
+    <Navbar :logo="logo_src" :alt="app_name" />
     <router-view/>
     <Footer />
   </div>
@@ -14,11 +14,33 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "JP Burger"
+    }
   }
 }
 </script>
 
-
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
